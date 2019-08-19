cf832870bac8b5c0c4874ef3b048d52171e9c2e4
gmo99 <35393320+gmo99@users.noreply.github.com>
2019-08-19 03:01:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var onRequest = new XMLHttpRequest();
 onRequest.open('GET', 'https://gmo99.github.io/test.json');
 onRequest.onload = function() {
     var data = JSON.parse(onRequest.responseText);
-    console.log(data[0]);
+    alert(data[0]);
 };
 onRequest.send();
