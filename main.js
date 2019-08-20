a2820340cb86d98c0ef15c55bcc410884c964b7b
gmo99 <35393320+gmo99@users.noreply.github.com>
2019-08-20 11:26:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function renderHtml(data) {
     var htmlString = "";
     
     for (let i = 0; i < data.length; i++) {
-        htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".<p>";
+        htmlString += `<p>${data[i].name} is a ${data[i].species}.<p>`;
     }
     
     dataContainer.insertAdjacentHTML('beforeend', htmlString);
