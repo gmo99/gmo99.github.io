9614dd3bae531ab96dfdd3398afada38504b4750
gmo99 <35393320+gmo99@users.noreply.github.com>
2019-08-20 11:07:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
+var dataContainer = document.getElementById("fetch-info");
 var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
     var XHR = new XMLHttpRequest();
     XHR.open('GET', 'https://gmo99.github.io/test.json');
     XHR.onload = function() {
-        var data = JSON.parse(XHR.responseText);
-        console.log(data[0]);
+        var serverData = JSON.parse(XHR.responseText);
+        renderHtml(serverData);
 };
     XHR.send();
 });
+
+function renderHtml(data) {
+    var htmlString ="";
+    
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".<p>";
+    }
+    
+    dataContainer.insertAdjacentHTML('beforeend', htmlString);
+}
